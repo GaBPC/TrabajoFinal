0e148d79ab77abaffcdd834e89b9f057bc4bcef5
GaBPC <gabprietoyt@gmail.com>
2016-10-12 18:03:01 -0300

    c - Exception para longitud de String c - Clase Material

---
+++ Project1/src/exceptions/LengthException.java
@@ -0,0 +1,14 @@
+package exceptions;
+
+public class LengthException extends Exception{
+    private String texto;
+    
+    public LengthException(String texto, String mensaje) {
+        super(mensaje);
+        this.texto = texto;
+    }
+    
+    public String getObject(){
+        return this.texto;
+    }
+}
