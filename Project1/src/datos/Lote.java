b17bb221deb2818c7efaa7db8e14d7e4bd6ee253
GaBPC <gabprietoyt@gmail.com>
2016-10-14 16:13:20 -0300

    Lo mismo que antes pero puse comit en vez de comit all

--- Project1/src/datos/Lote.java
+++ Project1/src/datos/Lote.java
@@ -4,110 +4,103 @@ import datos.estadosLote.Estado;
 
 import datos.estadosLote.Iniciado;
 
+import exceptions.ArgumentoIlegalException;
+
 import java.util.Calendar;
 import java.util.TreeSet;
 import java.util.ArrayList;
 
-public class Lote
-{
-  private String numeroPedido = null, numeroLote = null; ;
-  private Calendar fechaPedido = null, fechaEntregaVentas = null, fechaPropuestaProduccion = null, fechaDefinitiva =
-    null, fechaPedidoAceptado = null;
-  private String tipoMaquina = null;
-  private int cantProduccion = 0;
-  private Estado estadoActual;
+public class Lote {
+    private String numeroPedido = null, numeroLote = null; ;
+    private Calendar fechaPedido = null, fechaEntregaVentas = null, fechaPropuestaProduccion = null, fechaDefinitiva =
+        null, fechaPedidoAceptado = null;
+    private String tipoMaquina = null;
+    private int cantProduccion = 0;
+    private Estado estadoActual;
 
-  private TreeSet<Observacion> listaObservaciones;
+    private TreeSet<Observacion> listaObservaciones;
 
-  public Lote()
-  {
-    super();
-  }
+    public Lote() {
+        super();
+    }
 
-  public Lote(String numeroPedido, String numeroLote, Calendar fechaPedido, Calendar fechaEntregaVentas,
-              Calendar fechaPropuestaProduccion, Calendar fechaDefinitiva, Calendar fechaPedidoAceptado,
-              String tipoMaquina, int cantProduccion)
-  {
-    if (this.verificaNumeroPedido(numeroPedido))
-      this.numeroPedido = numeroPedido;
-    if(this.verificaNumeroLote(numeroLote))
-      this.numeroLote = numeroLote;
-    if(this.verficaTipoMaquina(tipoMaquina))
-      this.tipoMaquina = tipoMaquina;
-    if(this.verificaCantProduccion(cantProduccion))
-      this.cantProduccion = cantProduccion;
-    this.fechaPedido = fechaPedido;
-    this.fechaEntregaVentas = fechaEntregaVentas;
-    this.fechaPropuestaProduccion = fechaPropuestaProduccion;
-    this.fechaDefinitiva = fechaDefinitiva;
-    this.fechaPedidoAceptado = fechaPedidoAceptado;
+    public Lote(String numeroPedido, String numeroLote, Calendar fechaPedido, Calendar fechaEntregaVentas,
+                Calendar fechaPropuestaProduccion, Calendar fechaDefinitiva, Calendar fechaPedidoAceptado,
+                String tipoMaquina, int cantProduccion) throws ArgumentoIlegalException {
+        if (this.verificaNumeroPedido(numeroPedido))
+            this.numeroPedido = numeroPedido;
+        if (this.verificaNumeroLote(numeroLote))
+            this.numeroLote = numeroLote;
+        if (this.verficaTipoMaquina(tipoMaquina))
+            this.tipoMaquina = tipoMaquina;
+        if (this.verificaCantProduccion(cantProduccion))
+            this.cantProduccion = cantProduccion;
+        this.fechaPedido = fechaPedido;
+        this.fechaEntregaVentas = fechaEntregaVentas;
+        this.fechaPropuestaProduccion = fechaPropuestaProduccion;
+        this.fechaDefinitiva = fechaDefinitiva;
+        this.fechaPedidoAceptado = fechaPedidoAceptado;
 
-    this.listaObservaciones = new TreeSet<>();
+        this.listaObservaciones = new TreeSet<>();
 
-    this.estadoActual = new Iniciado(this);
-  }
+        this.estadoActual = new Iniciado(this);
+    }
 
-  private boolean verifica(String str)
-  {
-    boolean ret = false;
-    int num = Integer.parseInt(numeroPedido.substring(3).trim());
-    if (num >= 0 && num <= 999999)
-      ret = true;
-    return ret;
-  }
+    private boolean verifica(String str) throws ArgumentoIlegalException {
+        boolean ret = false;
+        int num = Integer.parseInt(str.substring(3).trim());
+        if (num >= 0 && num <= 999999)
+            ret = true;
+        else
+            throw new ArgumentoIlegalException("El numero esta fuera de rango", num);
+        return ret;
+    }
 
-  private boolean verificaNumeroPedido(String numeroPedido)
-  {
-    boolean ret = false;
-    String aux = numeroPedido.substring(0, 3);
-    if (aux.compareTo("PED") == 0)
-      ret = verifica(numeroPedido);
-    return ret;
-  }
+    private boolean verificaNumeroPedido(String numeroPedido) throws ArgumentoIlegalException {
+        boolean ret = false;
+        String aux = numeroPedido.substring(0, 3);
+        if (aux.compareTo("PED") == 0)
+            ret = verifica(numeroPedido);
+        else
+            throw new ArgumentoIlegalException("El numero de pedido no contiene \"PED\"", numeroPedido);
+        return ret;
+    }
 
-  private boolean verificaNumeroLote(String numeroLote)
-  {
-    boolean ret = false;
-    String aux = numeroLote.substring(0, 3);
-    if (aux.compareTo("LOT") == 0)
-      ret = verifica(numeroLote);
-    return ret;
-  }
-  
-  private boolean verficaTipoMaquina(String tipoMaquina)
-  {
-    return (tipoMaquina != null);
-  }
-  
-  private boolean verificaCantProduccion(int cantProduccion)
-  {
-    return (cantProduccion > 0 && cantProduccion < 999);
-  }
+    private boolean verificaNumeroLote(String numeroLote) throws ArgumentoIlegalException {
+        boolean ret = false;
+        String aux = numeroLote.substring(0, 3);
+        if (aux.compareTo("LOT") == 0)
+            ret = verifica(numeroLote);
+        else
+            throw new ArgumentoIlegalException("El numero de lote no contiene \"LOT\"", numeroLote);
+        return ret;
+    }
 
-  public void setEstadoActual(Estado estadoActual)
-  {
-    this.estadoActual = estadoActual;
-  }
+    private boolean verficaTipoMaquina(String tipoMaquina) {
+        return (tipoMaquina != null);
+    }
 
-  public Estado getEstadoActual()
-  {
-    return estadoActual;
-  }
+    private boolean verificaCantProduccion(int cantProduccion) {
+        return (cantProduccion > 0 && cantProduccion < 999);
+    }
 
-  public void agregarObservacion(Observacion obs)
-    throws Exception
-  {
-    this.estadoActual.agregarObservacion(obs);
-  }
+    public void setEstadoActual(Estado estadoActual) {
+        this.estadoActual = estadoActual;
+    }
 
-  public void aceptarLote()
-    throws Exception
-  {
-    this.estadoActual.aceptarLote();
-  }
+    public Estado getEstadoActual() {
+        return estadoActual;
+    }
 
-  public TreeSet<Observacion> getListaObservaciones()
-  {
-    return listaObservaciones;
-  }
+    public void agregarObservacion(Observacion obs) throws Exception {
+        this.estadoActual.agregarObservacion(obs);
+    }
+
+    public void aceptarLote() throws Exception {
+        this.estadoActual.aceptarLote();
+    }
+
+    public TreeSet<Observacion> getListaObservaciones() {
+        return listaObservaciones;
+    }
 }
--- Project1/src/datos/Material.java
+++ Project1/src/datos/Material.java
@@ -1,48 +1,48 @@
 package datos;
 
+import exceptions.ArgumentoIlegalException;
 import exceptions.LengthException;
 
 public class Material {
-  
+
     private String codigo = null;
     private String descripcion = null;
     private float cantidad = 0.0f;
-    
+
     public Material() {
         super();
     }
-    
-    private boolean verificaCodigo(String codigo)
-    {
-      boolean ret = false;
-      String aux = codigo.substring(0, 3);
-      if (aux.compareTo("MAT") == 0)
-      {
-        int num = Integer.parseInt(codigo.substring(3).trim());
-        if (num >= 0 && num <= 999999)
-          ret = true;
-      }
-      return ret;
-    }
-    
-    private boolean verificaDescripcion(String descripcion) throws LengthException
-  {
-      boolean ret = false;
-      if (descripcion.length() <= 100)
-        ret = true;
-      else
-        throw new LengthException(descripcion,"El texto tiene mas de 100 caracteres");
-      return ret;
-    }
-    
-    private boolean verificaCantidad(float cantidad)
-    {
-      return (cantidad > 0.0 && cantidad <= 999.9999);
+
+    private boolean verificaCodigo(String codigo) throws ArgumentoIlegalException {
+        boolean ret = false;
+        String aux = codigo.substring(0, 3);
+        if (aux.compareTo("MAT") == 0) {
+            int num = Integer.parseInt(codigo.substring(3).trim());
+            if (num >= 0 && num <= 999999)
+                ret = true;
+            else
+                throw new ArgumentoIlegalException("Numero fuera de rango", num);
+        }
+        else throw new ArgumentoIlegalException("El codigo no contiene \"MAT\"", codigo);
+        return ret;
     }
 
-    public void setCodigo(String codigo) {
-        if(this.verificaCodigo(codigo))
-          this.codigo = codigo;
+    private boolean verificaDescripcion(String descripcion) throws LengthException {
+        boolean ret = false;
+        if (descripcion.length() <= 100)
+            ret = true;
+        else
+            throw new LengthException(descripcion, "El texto tiene mas de 100 caracteres");
+        return ret;
+    }
+
+    private boolean verificaCantidad(float cantidad) {
+        return (cantidad > 0.0 && cantidad <= 999.9999);
+    }
+
+    public void setCodigo(String codigo) throws ArgumentoIlegalException {
+        if (this.verificaCodigo(codigo))
+            this.codigo = codigo;
     }
 
     public String getCodigo() {
@@ -50,7 +50,7 @@ public class Material {
     }
 
     public void setDescripcion(String descripcion) throws LengthException {
-        if(this.verificaDescripcion(descripcion))
+        if (this.verificaDescripcion(descripcion))
             this.descripcion = descripcion;
     }
 
@@ -59,8 +59,8 @@ public class Material {
     }
 
     public void setCantidad(float cantidad) {
-      if(this.verificaCantidad(cantidad))
-        this.cantidad = cantidad;
+        if (this.verificaCantidad(cantidad))
+            this.cantidad = cantidad;
     }
 
     public float getCantidad() {
